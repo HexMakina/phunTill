d59cdc45651c95f0646a87bab527126bcb5a7c4a
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-05-11 23:22:04 +0200

    fixed prop access failure + constant CASH & CARD

--- src/PaymentMethods.php
+++ src/PaymentMethods.php
@@ -9,26 +9,27 @@ namespace HexMakina\phunTill;
 
 class PaymentMethods
 {
-
+    public const CASH = 'Cash';
+    public const CARD = 'Bancontact';
+    
     private int $cash_id;
     private int $card_id;
 
     public function __construct(POSAPI $api)
     {
         $payment_methods = $api->get('payment-info');
-
         foreach ($payment_methods->asArray() as $method) {
-            if (!isset($this->cash_id) && $method->name == 'Cash')
+            if (!isset($this->cash_id) && $method->name == self::CASH)
                 $this->cash_id = (int)$method->id;
 
-            if (!isset($this->card_id) && $method->name == 'Card')
+            if (!isset($this->card_id) && $method->name == self::CARD)
                 $this->card_id = (int)$method->id;
         }
 
-        if (is_null($this->cash_id))
+        if (empty($this->cash_id))
             throw new phunTillException('Payment method "cash" is not available');
 
-        if (is_null($this->card_id))
+        if (empty($this->card_id))
             throw new phunTillException('Payment method "card" is not available');
     }
 
