4b96c0f4e142e3ff08a6467c00ecd9c9a98ad1f8
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-05-11 23:16:47 +0200

    PSR12

--- src/PaymentMethods.php
+++ src/PaymentMethods.php
@@ -7,41 +7,38 @@
 
 namespace HexMakina\phunTill;
 
-class PaymentMethods {
+class PaymentMethods
+{
 
     private int $cash_id;
     private int $card_id;
 
     public function __construct(POSAPI $api)
     {
-      $payment_methods = $api->get('payment-info');
+        $payment_methods = $api->get('payment-info');
 
-      foreach($payment_methods->asArray() as $method)
-      {
-        if(!isset($this->cash_id) && $method->name == 'Cash')
-          $this->cash_id = (int)$method->id;
+        foreach ($payment_methods->asArray() as $method) {
+            if (!isset($this->cash_id) && $method->name == 'Cash')
+                $this->cash_id = (int)$method->id;
 
-        if(!isset($this->card_id) && $method->name == 'Card')
-          $this->card_id = (int)$method->id;
-      }
+            if (!isset($this->card_id) && $method->name == 'Card')
+                $this->card_id = (int)$method->id;
+        }
 
-      if(is_null($this->cash_id))
-          throw new phunTillException('Payment method "cash" is not available');
+        if (is_null($this->cash_id))
+            throw new phunTillException('Payment method "cash" is not available');
 
-      if(is_null($this->card_id))
-          throw new phunTillException('Payment method "card" is not available');
-
+        if (is_null($this->card_id))
+            throw new phunTillException('Payment method "card" is not available');
     }
 
-    public function cash() : int
+    public function cash(): int
     {
         return $this->cash_id;
     }
 
-    public function card() : int
+    public function card(): int
     {
         return $this->card_id;
     }
-
-
 }
