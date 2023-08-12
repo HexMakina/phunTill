5514db81b4db46d69a2f2ffe2c9d3779e6a2317f
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-08-12 21:43:34 +0200

    added setClient & setNote

--- src/Order.php
+++ src/Order.php
@@ -9,14 +9,16 @@ namespace HexMakina\phunTill;
 
 class Order {
 
+    public $clientName;
     public $tableNumber;
     public $tablePart;
     public $items = [];
+    public $takeAwayNotes;
 
     public function __construct(Table $table, $items = []) {
         $this->tableNumber = $table->getNumber();
         $this->tablePart = $table->getPart();
-        $this->items = $items;
+        $this->items = is_array($items) ? $items : [];
     }
 
     public function tableNumber()
@@ -37,6 +39,10 @@ class Order {
 
         return $item->number();
     }
+    
+    public function setClient(string $name){
+        $this->clientName = $name;
+    }
 
     public function addNormalArticle($articleId, $quantity): int
     {
@@ -51,6 +57,10 @@ class Order {
       $message->setText($text);
 
       return $this->addItem($message);
+    }
+
+    public function setNote(string $text){
+      $this->takeAwayNotes = $text;
     }
 
     public function items():array
